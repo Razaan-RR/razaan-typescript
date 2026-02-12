9c98479454368b062d9b752df0e326efdfab5420
Razaan <razaanreza0705@gmail.com>
2026-02-12 17:39:16 +0600

    problems 1-7 added

--- src/problems.ts
+++ src/problems.ts
@@ -10,6 +10,9 @@ console.log(formatString('hello'))
 console.log(formatString('hello', true))
 console.log(formatString('hello', false))
 
+
+
+
 function filterByRating(items: { title: string; rating: number }[]): { title: string; rating: number }[] {
   const result=[]
   for(const item of items){
@@ -26,3 +29,111 @@ const books = [
 ];
 const filteredBooks = filterByRating(books)
 console.log(filteredBooks)
+
+
+
+
+function concatenateArrays<T>(...arrays: T[][]): T[]{
+  let result: T[] = []
+  for (let arr of arrays){
+    result = result.concat(arr)
+  }
+  return result
+}
+const output = concatenateArrays([1, 2], [3, 4], [5])
+console.log(output)
+
+
+
+
+class Vehicle{
+  private make:string
+  private year:number
+  constructor(make:string, year:number){
+    this.make=make
+    this.year=year
+  }
+  getInfo():string{
+    return `Make: ${this.make}, Year: ${this.year}`
+  }
+}
+class Car extends Vehicle {
+  private model:string
+  constructor(make:string, year:number, model:string) {
+    super(make,year)
+    this.model=model
+  }
+  getModel():string{
+    return `Model: ${this.model}`
+  }
+}
+const myCar = new Car("Toyota", 2020, "Corolla")
+console.log(myCar.getInfo())  
+console.log(myCar.getModel())  
+
+
+
+
+function processValue(value: string | number): number{
+  if(typeof value==='string'){
+    return value.length
+  } 
+  else{
+    return value*2
+  }
+}
+console.log(processValue("hello")) 
+console.log(processValue(10)) 
+
+
+
+
+interface Product {
+  name: string
+  price: number
+}
+function getMostExpensiveProduct(products: Product[]): Product | null{
+  let mostExpensive=products[0]!
+  if(products.length===0){
+    return null 
+  }
+  for(let product of products){
+    if(product.price>mostExpensive.price){
+      mostExpensive=product 
+    }
+  }
+  return mostExpensive 
+}
+const products = [
+  { name: "Pen", price: 10 },
+  { name: "Notebook", price: 25 },
+  { name: "Bag", price: 50 }
+]
+console.log(getMostExpensiveProduct(products))
+
+
+
+
+enum Day {
+  Monday,
+  Tuesday,
+  Wednesday,
+  Thursday,
+  Friday,
+  Saturday,
+  Sunday
+}
+function getDayType(day: Day): string{
+  if(day===Day.Saturday || day===Day.Sunday) {
+    return "Weekend"
+  } 
+  else{
+    return "Weekday"
+  }
+}
+console.log(getDayType(Day.Monday))   
+console.log(getDayType(Day.Sunday))   
+
+
+
+
